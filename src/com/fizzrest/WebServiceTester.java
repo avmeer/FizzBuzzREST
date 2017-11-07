a56400d172fc8fb53a071928d488f5f4359be232
Alan <alan.j.vandermeer@gmail.com>
2017-11-07 14:05:47 -0700

    Fixed testing and added more tests

--- src/com/fizzrest/WebServiceTester.java
+++ src/com/fizzrest/WebServiceTester.java
@@ -24,26 +24,61 @@ public class WebServiceTester  {
       tester.init();
       //test the Json is created and returned properly
       tester.testFizzbuzz15();
+      
+      tester.testFizzbuzz50();
+      
+      tester.testFizzbuzz100();
    }
    
    private void testFizzbuzz15(){
 	   
-	  ArrayList<Integer> expectedBuzz = new ArrayList<Integer>(Arrays.asList(5,10));
-	  ArrayList<Integer> expectedFizz= new ArrayList<Integer>(Arrays.asList(3,6,9,12));
-	  ArrayList<Integer> expectedFizzBuzz= new ArrayList<Integer>(Arrays.asList(15));
-	  
-      FizzBuzz fizz = client
+	  String expectedJson = "{\"Buzz\":[5,10],\"Fizz\":[3,6,9,12],\"FizzBuzz\":[15]}";
+	   
+      String json = client
          .target("http://localhost:8080/fizzbuzz/15")
          .request(MediaType.APPLICATION_JSON)
-         .get(FizzBuzz.class);
+         .get(String.class);
 
       String result = FAIL;
       
-      if(fizz.getBuzz().equals(expectedBuzz) && fizz.getFizz().equals(expectedFizz) && fizz.getFizzBuzz().equals(expectedFizzBuzz)){
+      if(json.compareTo(expectedJson) == 0){
          result = PASS;
       }
       System.out.println("Test case name: test input 15, Result: " + result );
-      System.out.println(fizz.getFizz());
+   }
+   
+   private void testFizzbuzz50(){
+	   
+	  String expectedJson = "{\"Buzz\":[5,10,20,25,35,40,50],\"Fizz\":[3,6,9,12,18,21,24,27,33,36,39,42,48],\"FizzBuzz\":[15,30,45]}";
+	   
+      String json = client
+         .target("http://localhost:8080/fizzbuzz/50")
+         .request(MediaType.APPLICATION_JSON)
+         .get(String.class);
+
+      String result = FAIL;
+      
+      if(json.compareTo(expectedJson) == 0){
+         result = PASS;
+      }
+      System.out.println("Test case name: test input 50, Result: " + result );
+   }
+   
+   private void testFizzbuzz100(){
+	   
+	  String expectedJson = "{\"Buzz\":[5,10,20,25,35,40,50,55,65,70,80,85,95,100],\"Fizz\":[3,6,9,12,18,21,24,27,33,36,39,42,48,51,54,57,63,66,69,72,78,81,84,87,93,96,99],\"FizzBuzz\":[15,30,45,60,75,90]}";
+	   
+      String json = client
+         .target("http://localhost:8080/fizzbuzz/100")
+         .request(MediaType.APPLICATION_JSON)
+         .get(String.class);
+
+      String result = FAIL;
+      
+      if(json.compareTo(expectedJson) == 0){
+         result = PASS;
+      }
+      System.out.println("Test case name: test input 100, Result: " + result );
    }
  
 }
