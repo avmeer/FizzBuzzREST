71dc23fe5337de80c9ea0b0a5b3072265ee9c405
Alan <alan.j.vandermeer@gmail.com>
2017-11-07 12:46:01 -0700

    Initial commit. Prints number from GET request

---
+++ src/com/fizzrest/FizzBuzzService.java
@@ -0,0 +1,19 @@
+package com.fizzrest;  
+
+import java.util.List; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType;  
+@Path("/") 
+
+public class FizzBuzzService {  
+   
+   @GET
+   @Path("/{upperBound}")
+   @Produces(MediaType.APPLICATION_JSON)
+   public int getUser(@PathParam("upperBound") int upperBound){
+	  return upperBound;
+   }
+}
